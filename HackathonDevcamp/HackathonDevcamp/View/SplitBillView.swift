81b660524cb90a58af6057a1adc33b24036db040
Maria Berliana <mariabyp1617@gmail.com>
2023-12-08 19:34:30 +0700

    Add friends page

--- HackathonDevcamp/HackathonDevcamp/View/SplitBillView.swift
+++ HackathonDevcamp/HackathonDevcamp/View/SplitBillView.swift
@@ -16,7 +16,7 @@ struct TextFieldClearButton: ViewModifier {
             
             if !text.isEmpty {
                 Button(
-                    action: { self.text = "" },
+                    action: { self.text = ""},
                     label: {
                         Image(systemName: "x.circle.fill")
                             .foregroundColor(.gray)
@@ -35,10 +35,22 @@ struct PlusButton: View {
         }
     }
 }
+struct APIResponse: Codable {
+    let data: UserData?
+}
+
+struct UserData: Codable {
+    let name: String
+    let phone: String
+    // Add other properties as needed
+}
 
 struct SplitBillView: View {
     @State private var nomorUser: String = ""
+    @State private var namaUser: String = ""
     @State private var isEditing: Bool = false
+    @State private var friends: [(name: String, phoneNumber: String)] = []
+    
     var body: some View {
         NavigationView{
             VStack{
@@ -52,6 +64,7 @@ struct SplitBillView: View {
                         Text("Tambahkan Teman")
                             .fontWeight(.bold)
                             .foregroundColor(Color("Dark2"))
+                            .font(.system(size: 20))
                         
                     }.padding(.vertical, 20.0)
                     ZStack{
@@ -89,13 +102,72 @@ struct SplitBillView: View {
                         
                     }
                 }
-                Text("Tambah Teman")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("Hijau1"))
+                HStack {
+                    Button(action: {
+                        fetchUserData()
+                        
+                    }) {
+                        Text("Tambah Teman")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("Hijau1"))
+                    }
+                }
+                
+                VStack(alignment: .leading){
+                    Text("List")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                    
+                }.padding(.leading, -180.0)
+                ForEach(friends, id: \.phoneNumber) { friend in
+                    HStack{
+                        Text(friend.name)
+                            .padding(.leading, 30.0)
+                            .foregroundColor(Color("Dark1"))
+                        Text("-")
+                        Text(friend.phoneNumber)
+                            .foregroundColor(Color("Dark1"))
+                        
+                        Spacer()
+                        Image(systemName: "trash.fill")
+                            .foregroundColor(.red)
+                            .padding(.trailing, 30.0)
+                    }
+                    .frame(width: 365.0, height: 60.0)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color("Hijau1"), lineWidth: 2)
+                    )
+                }
+                
                 Spacer()
             }
             .padding()
         }.navigationBarBackButtonHidden(true)
+    }
+    func fetchUserData() {
+        guard let url = URL(string: "http://localhost:8080/api/user/\(nomorUser)") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let apiResponse = try decoder.decode(APIResponse.self, from: data)
+                
+                if let userData = apiResponse.data {
+                    DispatchQueue.main.async {
+                        let newFriend = (name: userData.name, phoneNumber: userData.phone)
+                        friends.append(newFriend)
+                    }
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 }
 
