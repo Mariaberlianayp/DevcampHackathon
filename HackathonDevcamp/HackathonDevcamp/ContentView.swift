2bf19d32ebb6ef23c7eee37f7e75d1ecb502175b
Maria Berliana <mariabyp1617@gmail.com>
2023-12-08 11:21:16 +0700

    Create Project

---
+++ HackathonDevcamp/HackathonDevcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackathonDevcamp
+//
+//  Created by Maria Berliana on 08/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackathonDevcamp/HackathonDevcamp/HackathonDevcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonDevcampApp.swift
+//  HackathonDevcamp
+//
+//  Created by Maria Berliana on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonDevcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
