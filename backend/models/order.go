e9e95e4c4d1709407c8623e59af9676efe909d0f
Kevin Dharmawan <kevindharmawan2002@gmail.com>
2023-12-08 15:03:06 +0700

    Implement cart

--- backend/models/order.go
+++ backend/models/order.go
@@ -1,13 +1,20 @@
 package models
 
 type Order struct {
-	ID       uint      `json:"id" gorm:"primary_key"`
-	UserID   uint      `json:"-"`
-	User     User      `json:"user" gorm:"foreignKey:UserID"`
-	Products []Product `json:"products" gorm:"many2many:order_products"`
-	Quantity int       `json:"quantity" gorm:"not null"`
-	Payments []Payment `json:"payments" gorm:"foreignKey:OrderID"`
-	Status   string    `json:"status" gorm:"not null"`
+	ID            uint           `json:"id" gorm:"primary_key"`
+	UserID        uint           `json:"-"`
+	User          User           `json:"user" gorm:"foreignKey:UserID"`
+	OrderProducts []OrderProduct `json:"products" gorm:"foreignKey:OrderID"`
+	Payments      []Payment      `json:"payments" gorm:"foreignKey:OrderID"`
+	Status        string         `json:"status" gorm:"not null"`
+}
+
+type OrderProduct struct {
+	ID        uint    `json:"id" gorm:"primary_key"`
+	OrderID   uint    `json:"-"`
+	ProductID uint    `json:"-"`
+	Product   Product `json:"product" gorm:"foreignKey:ProductID"`
+	Quantity  int     `json:"quantity" gorm:"not null"`
 }
 
 type Payment struct {
